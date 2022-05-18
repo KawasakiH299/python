ce1209c8a9aa4a919b00132a93d8cf942fe6c9dd
KawasakiH299 <105109499+KawasakiH299@users.noreply.github.com>
2022-05-18 19:44:36 +0800

    Add files via upload
    datatime!

---
+++ datatime.py
@@ -0,0 +1,9 @@
+
+import datetime
+now_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+year = datetime.datetime.now().strftime('%Y')
+month = datetime.datetime.now().strftime('%Y-%m')
+day = datetime.datetime.now().strftime('%Y-%m-%d')
+hour = datetime.datetime.now().strftime("%H-%M-%S")
+hour_1 = datetime.datetime.now().strftime("%S")
+print(hour)
